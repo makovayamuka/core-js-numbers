e224c7053afcda074124ed71b89bc587f7105503
makovayamuka <skaZcka25@gmail.com>
2023-11-17 13:47:49 +0000

    docs: get the task done

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -235,8 +235,13 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const number = +value;
+
+  if (!Number.isNaN(number)) {
+    return number;
+  }
+  return def;
 }
 
 /**
@@ -250,8 +255,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
@@ -267,8 +272,18 @@ function getCube(/* num */) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index <= 1) {
+    return index;
+  }
+  let a = 0;
+  let b = 1;
+  for (let i = 2; i <= index; i += 1) {
+    const temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
 }
 
 /**
